8e9829c87c07c04840a7fd5361a3ee8475157486
David Castaneda <Davidcas7@hotmail.com>
2026-03-29 11:32:30 -0400

    Initial Commit

---
+++ Tally/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tally
+//
+//  Created by David Castaneda on 3/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tally/TallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyApp.swift
+//  Tally
+//
+//  Created by David Castaneda on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
